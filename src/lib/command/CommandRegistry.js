d014369ee90f08a12002e969d7ea27a99e8e5e9f
Zack <Zack>
2016-10-03 06:32:52 -0500

    Add error on shared command aliases

--- src/lib/command/CommandRegistry.js
+++ src/lib/command/CommandRegistry.js
@@ -16,6 +16,17 @@ export default class CommandRegistry extends Map
 		if (super.has(command.name)) throw new Error(`A command with the name "${command.name}" already exists.`);
 		command.register();
 		super.set(key, command);
+		this.forEach(a =>
+		{
+			a.aliases.forEach(b =>
+			{
+				let duplicates = this.commandArray().filter(c => c.aliases.includes(b) && c !== a);
+				if (duplicates.length > 0)
+				{
+					throw new Error(`Commands may not share aliases: ${duplicates[0].name}, ${a.name} (shared alias: ${b})`);
+				}
+			});
+		});
 	}
 
 	commandArray()
