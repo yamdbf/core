0169da9e0e55b3b6b6b6cff1cb735698e8d6d955
Zack Campbell <zajrik@gmail.com>
2016-10-17 00:32:11 -0500

    Add version export, Update docs with exported modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use babel';
 'use strict';
 
+import path from 'path';
+
 import _Bot from './lib/bot/Bot';
 import _GuildStorage from './lib/storage/GuildStorage';
 import _GuildStorageLoader from './lib/storage/GuildStorageLoader';
@@ -10,16 +12,41 @@ import _Command from './lib/command/Command';
 import _CommandLoader from './lib/command/CommandLoader';
 import _CommandRegistry from './lib/command/CommandRegistry';
 import _CommandDispatcher from './lib/command/CommandDispatcher';
+import _Util from './lib/Util';
 
+/** @exports Bot */
 export const Bot = _Bot;
+export default Bot;
+
+/** @exports GuildStorage */
 export const GuildStorage = _GuildStorage;
+
+/** @exports GuildStorageLoader */
 export const GuildStorageLoader = _GuildStorageLoader;
+
+/** @exports GuildStorageRegistry */
 export const GuildStorageRegistry = _GuildStorageRegistry;
+
+/** @exports LocalStorage */
 export const LocalStorage = _LocalStorage;
+
+/** @exports Command */
 export const Command = _Command;
+
+/** @exports CommandLoader */
 export const CommandLoader = _CommandLoader;
+
+/** @exports CommandRegistry */
 export const CommandRegistry = _CommandRegistry;
+
+/** @exports CommandDispatcher */
 export const CommandDispatcher = _CommandDispatcher;
+
+/** @exports Util */
+export const Util = _Util;
+
+/** @exports version */
+export const version = require(path.join(__dirname, '..', 'package')).version;
 
 /** @external {Client} See: {@link http://hydrabolt.github.io/discord.js/#!/docs/tag/master/class/Client} */
 /** @external {Collection} See: {@link http://hydrabolt.github.io/discord.js/#!/docs/tag/master/class/Collection} */
