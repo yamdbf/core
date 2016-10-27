c0d410b37e539899d5dfda216481496ad12997e5
Zack Campbell <zajrik@gmail.com>
2016-10-27 13:10:53 -0500

    Update docs

--- src/lib/bot/Bot.js
+++ src/lib/bot/Bot.js
@@ -141,9 +141,9 @@ export default class Bot extends Client
 		this.commands = new CommandRegistry();
 
 		/**
-		 * @typedef {Object} defaultGuildSettings - The default settings to apply to new guilds.
+		 * @typedef {Object} DefaultGuildSettings - The default settings to apply to new guilds.
 		 * Stored under the key <code>'defaultGuildSettings'</code> in {@link Bot#storage}
-		 * @property {string} [prefix='/'] - Prefix to prepend commands
+		 * @property {string} prefix='/' - Prefix to prepend commands
 		 * @property {Array} [disabledGroups=[]] - Command groups to ignore
 		 */
 
