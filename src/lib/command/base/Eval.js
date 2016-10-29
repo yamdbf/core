4765408be25979d80b0826e3d19c63b93b73096f
Zack Campbell <zajrik@gmail.com>
2016-10-29 07:07:42 -0500

    Remove source command on eval calls for selfbots

--- src/lib/command/base/Eval.js
+++ src/lib/command/base/Eval.js
@@ -22,6 +22,7 @@ export default class Eval extends Command
 
 	async action(message, args)
 	{
+		if (this.bot.selfbot) message.delete();
 		const code = args.join(' ');
 		if (!code) return this._respond(message, '**ERROR:** ```xl\nNo code provided to evaluate.\n```');
 
