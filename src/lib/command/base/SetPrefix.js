5fe58a481d0e1872655e747a446fc4ea9c6956da
Zack Campbell <zajrik@gmail.com>
2016-12-29 16:00:54 -0600

    Update SetPrefix command

--- src/lib/command/base/SetPrefix.js
+++ src/lib/command/base/SetPrefix.js
@@ -9,9 +9,9 @@ export default class SetPrefix extends Command
 	{
 		super(bot, {
 			name: 'setprefix',
-			description: 'Set the bot command prefix for this guild.',
+			description: 'Set or check the bot command prefix for this guild',
 			aliases: ['prefix'],
-			usage: '<prefix>setprefix <text>',
+			usage: '<prefix>setprefix [text]',
 			extraHelp: 'Prefixes may be 1-10 characters in length and may not include backslashes or backticks. Set the prefix to "noprefix" to allow commands to be called without a prefix.',
 			group: 'base',
 			permissions: ['ADMINISTRATOR']
@@ -23,29 +23,29 @@ export default class SetPrefix extends Command
 		if (!args[0])
 		{
 			this._respond(message, `${this.bot.getPrefix(message.guild)
-				? 'Current prefix is \'' + this.bot.getPrefix(message.guild) + '\''// eslint-disable-line prefer-template
+				? `Current prefix is \`${this.bot.getPrefix(message.guild)}\``
 				: 'There is currently no prefix.'}`)
-				.then(response => response.delete(5 * 1000));
+				.then(res => res.delete(5e3));
 			return;
 		}
 		if (args[0].length > 10)
 		{
 			this._respond(message, `Prefixes may only be up to 10 chars in length.`)
-				.then(response => response.delete(5 * 1000));
+				.then(res => res.delete(5e3));
 			return;
 		}
 		if (/[\\`]/.test(args[0]))
 		{
 			this._respond(message, `Prefixes may not contain backticks or backslashes.`)
-				.then(response => response.delete(5 * 1000));
+				.then(res => res.delete(5e3));
 			return;
 		}
 		if (args[0] === 'noprefix') args[0] = '';
 
 		if (this.bot.selfbot) this.bot.guildStorages.forEach(guild => guild.setSetting('prefix', args[0]));
 		else this.bot.guildStorages.get(message.guild).setSetting('prefix', args[0]);
-		this._respond(message, `${args[0] === '' ? 'Command prefix removed.'
-			: 'Command prefix set to "' + args[0] + '"'}`) // eslint-disable-line prefer-template
-			.then(response => response.delete(5 * 1000));
+		this._respond(message, args[0] === '' ? 'Command prefix removed.'
+			: `Command prefix set to \`${args[0]}\``)
+			.then(res => res.delete(5e3));
 	}
 }
