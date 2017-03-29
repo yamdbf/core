c4ed07451158099e903a2f5bc769b70959855bda
Zack Campbell <zajrik@gmail.com>
2017-03-29 04:15:00 -0500

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,19 @@ import * as path from 'path';
 
 export { Bot } from './lib/bot/Bot';
 export { Command } from './lib/command/Command';
+export { CommandDispatcher } from './lib/command/CommandDispatcher';
 export { CommandLoader } from './lib/command/CommandLoader';
 export { CommandRegistry } from './lib/command/CommandRegistry';
-export { CommandDispatcher } from './lib/command/CommandDispatcher';
-export { GuildStorage } from './lib/storage/GuildStorage';
+export { GuildSettings } from './lib/storage/GuildSettings';
 export { GuildStorageLoader } from './lib/storage/GuildStorageLoader';
-export { GuildStorageRegistry } from './lib/storage/GuildStorageRegistry';
-export { LocalStorage } from './lib/storage/LocalStorage';
+export { KeyedStorage } from './lib/storage/KeyedStorage';
 export { Middleware } from './lib/command/middleware/Middleware';
-export { RateLimiter } from './lib/command/RateLimiter';
 export { RateLimit } from './lib/command/RateLimit';
+export { RateLimiter } from './lib/command/RateLimiter';
 
 export { StorageProvider } from './lib/storage/StorageProvider';
+export { StorageFactory } from './lib/storage/StorageFactory';
 export { JSONProvider } from './lib/storage/JSONProvider';
-export { GuildSettings } from './lib/storage/GuildSettings';
-export { ClientStorage } from './lib/types/ClientStorage';
 
 import * as CommandDecorators from './lib/command/CommandDecorators';
 export { CommandDecorators }
@@ -25,14 +23,19 @@ export { Time } from './lib/Time';
 export { Util } from './lib/Util';
 
 export { ArgOpts } from './lib/types/ArgOpts';
+export { BaseCommandName } from './lib/types/BaseCommandName';
 export { BotOptions } from './lib/types/BotOptions';
+export { ClientStorage } from './lib/types/ClientStorage';
 export { CommandInfo } from './lib/types/CommandInfo';
 export { DefaultGuildSettings } from './lib/types/DefaultGuildSettings';
+export { Difference } from './lib/types/Difference';
 export { ExpectArgType } from './lib/types/ExpectArgType';
 export { Guild } from './lib/types/Guild';
+export { GuildStorage } from './lib/types/GuildStorage';
 export { Message } from './lib/types/Message';
+export { MiddlewareFunction } from './lib/types/MiddlewareFunction';
 export { ResolveArgType } from './lib/types/ResolveArgType';
-export { BaseCommandName } from './lib/types/BaseCommandName';
+export { StorageProviderConstructor } from './lib/types/StorageProviderConstructor';
 
 export const version: string = require(path.join(__dirname, '..', 'package')).version;
 
