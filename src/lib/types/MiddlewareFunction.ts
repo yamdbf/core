50700802102d62dfce37512c012d5e76b18603e3
Zack Campbell <zajrik@gmail.com>
2017-04-03 03:24:10 -0500

    Update docs

--- src/lib/types/MiddlewareFunction.ts
+++ src/lib/types/MiddlewareFunction.ts
@@ -3,9 +3,9 @@
  * does anything with them, and returns an array where the first item is the Message object and the
  * second item is the array of args.
  *
- * It should be noted that the command dispatcher will attempt
- * to bind the called Command instance to the middleware function, so if it is not an arrow function
- * `this` within a middleware function will be the Command instance at runtime
+ * It should be noted that the command dispatcher will attempt to bind the called Command instance
+ * to the middleware function, so if it is not an arrow function `this` within a middleware
+ * function will be the Command instance at runtime
  */
 
 import { Message } from './Message';
--- src/lib/types/StorageProviderConstructor.ts
+++ src/lib/types/StorageProviderConstructor.ts
@@ -1,6 +1,7 @@
 /**
- * @typedef {Function} StorageProviderConstructor Any class that extends and implements {@link StorageProvider}
- * and provides an interface with a storage medium allowing data to be stored, retrieved, and manipulated. *Not
+ * @typedef {Function} StorageProviderConstructor Any class that extends and
+ * implements {@link StorageProvider} and provides an interface with a storage
+ * medium allowing data to be stored, retrieved, and manipulated. *Not
  * to be confused with an **instance** of a storage provider.*
  */
 
