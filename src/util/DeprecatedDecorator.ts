ef7cf41fe9a4796fa8d2c2e21e23abd7cd2f5d1b
Zack Campbell <zajrik@gmail.com>
2017-04-15 03:50:23 -0500

    Add deprecated decorator
    For internal use

---
+++ src/util/DeprecatedDecorator.ts
@@ -0,0 +1,22 @@
+import { Logger } from './logger/Logger';
+const logger: Logger = Logger.instance();
+
+/**
+ * Logs a deprecation warning for the decorated class method
+ * @private
+ * @param {string} [message] Method deprecation message
+ */
+export function deprecated<T extends Function>(message?: string): MethodDecorator
+{
+	return function(target: T, key: string, descriptor: PropertyDescriptor): PropertyDescriptor
+	{
+		if (!descriptor) descriptor = Object.getOwnPropertyDescriptor(target, key);
+		const original: any = descriptor.value;
+		descriptor.value = function(...args: any[]): any
+		{
+			logger.warn('Deprecation', message || `${target.constructor.name}#${key}() is deprecated and will be removed in a future release.`);
+			return original.apply(this, args);
+		};
+		return descriptor;
+	};
+}
