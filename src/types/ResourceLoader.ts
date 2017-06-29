d60bf6a3ec060025acc1f634642f52b246ddf0ed
Zack Campbell <zajrik@gmail.com>
2017-06-29 17:39:07 -0500

    Update ResourceLoader docs

--- src/types/ResourceLoader.ts
+++ src/types/ResourceLoader.ts
@@ -1,7 +1,8 @@
 /**
  * @typedef {Function} ResourceLoader Represents a function assigned to
- * a specific language that takes a string key and a {@link TemplateData}
- * object and returns a localized string for that language if it exists
+ * a specific language that takes a string key and an optional
+ * {@link TemplateData} object and returns a localized string for
+ * that language if it exists
  */
 
 import { TemplateData } from './TemplateData';
