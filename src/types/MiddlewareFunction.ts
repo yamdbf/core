28135f19bc07a0fa5d1c63da8c7855a7e304cbee
Zack Campbell <zajrik@gmail.com>
2017-07-05 20:35:23 -0500

    Update MiddlewareFunction docs

--- src/types/MiddlewareFunction.ts
+++ src/types/MiddlewareFunction.ts
@@ -1,19 +1,18 @@
 /**
  * @typedef {Function} MiddlewareFunction A function that takes a Message object and an array of args,
- * does anything with them, and returns {@link Tuple} where the first item is the Message object and the
+ * does anything with them, and returns a {@link Tuple} where the first item is the Message object and the
  * second item is the array of args
  *
- * Method signature:
  * ```
  * (Message, any[]) => [Message, any[]]
  * ```
  *
- * Can be async as long as the returned promise resolves with
- * the array containing the message object and the args array as expected.
+ * Can be async as long as the returned promise resolves with the tuple
+ * containing the message object and the args array as expected.
  *
- * It should be noted that the command dispatcher will attempt to bind the called Command instance
- * to the middleware function, so if it is not an arrow function `this` within a middleware
- * function will be the Command instance at runtime
+ * >**Note:** The command dispatcher will attempt to bind the Command instance to the middleware
+ * function when called, so `this` within a middleware function (if it is not an arrow function)
+ * will be the Command instance at runtime
  */
 
 import { Message } from './Message';
