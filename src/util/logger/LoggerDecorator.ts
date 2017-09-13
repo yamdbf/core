a5ef733a0b8bd1d1f03e5c74dde477f4e4bf4e26
Zack Campbell <zajrik@gmail.com>
2017-09-13 14:30:54 -0500

    Fix logger decorator having incorrect comment in intellisense
    Now it has *no* description, but at least it still has the docs on the site.

--- src/util/logger/LoggerDecorator.ts
+++ src/util/logger/LoggerDecorator.ts
@@ -1,30 +1,30 @@
 import { Logger } from './Logger';
 
-/**
- * Represents a type that has a `logger` property
- * containing the Logger singleton instance
- */
-// export type Loggable<T> = T & { logger: Logger };
+// /**
+//  * Represents a type that has a `logger` property
+//  * containing the Logger singleton instance
+//  */
+// // export type Loggable<T> = T & { logger: Logger };
 
-/**
- * Class decorator that transforms a class to Loggable<T>.
- * Regrettably works but is not properly picked up by intellisense
- * at this point in time, meaning compiler errors when attempting
- * to access the `logger` property on the decorated class.
- * Maybe someday.
- *
- * Example:
- * ```
- * &#64loggable
- * class Foo { }
- * ```
- */
-// export function loggable<T extends Function>(target: T): Loggable<T>
-// {
-// 	Object.defineProperty(target.prototype, 'logger',
-// 		{ value: Logger.instance });
-// 	return <Loggable<T>> target;
-// }
+// /**
+//  * Class decorator that transforms a class to Loggable<T>.
+//  * Regrettably works but is not properly picked up by intellisense
+//  * at this point in time, meaning compiler errors when attempting
+//  * to access the `logger` property on the decorated class.
+//  * Maybe someday.
+//  *
+//  * Example:
+//  * ```
+//  * &#64loggable
+//  * class Foo { }
+//  * ```
+//  */
+// // export function loggable<T extends Function>(target: T): Loggable<T>
+// // {
+// // 	Object.defineProperty(target.prototype, 'logger',
+// // 		{ value: Logger.instance });
+// // 	return <Loggable<T>> target;
+// // }
 
 export function logger(key: string): PropertyDecorator;
 export function logger<T>(target: T, key: string): void;
