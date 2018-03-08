6e02c5c5bc63544f5c0d7b0be92c30826fe69ee5
Zack Campbell <zajrik@gmail.com>
2018-03-08 10:56:25 -0600

    Add shard ID to base transport logs when sharding

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -90,6 +90,9 @@ export class Client extends Discord.Client
 		super(clientOptions);
 		Reflect.defineMetadata('YAMDBFClient', true, this);
 
+		// Hook logger to provide shard ID in base transport logs
+		if (this.shard) Logger._shard = this.shard.id;
+
 		this._token = options.token;
 
 		/**
