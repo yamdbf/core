9aa5c3646c474c880f8448e22cc9686b5094aea2
Zackary Campbell <zajrik@zajriksrv>
2018-03-17 10:08:49 +0100

    Build YAMDBF prebuilt: ffcdca2a9cd7b0fab69549463f76f83e90417523

--- bin/command/Command.js
+++ bin/command/Command.js
@@ -116,12 +116,6 @@ class Command {
          * @name Command#external
          * @type {boolean}
          */
-        /**
-         * Whether or not this command is disabled and unable to be called
-         * currently
-         * @name Command#disabled
-         * @type {boolean}
-         */
         // Middleware function storage for the Command instance
         this._middleware = [];
         this._initialized = false;
