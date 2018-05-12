fd6a524df699302dcc1ab31c9cdeae9be8308572
Zack Campbell <zajrik@gmail.com>
2018-05-12 02:27:26 -0500

    Only clear reactions if they already exist for compact mode response

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -347,7 +347,7 @@ export class Command<T extends Client = Client>
 			&& typeof options.button !== 'undefined'
 			&& (await message.guild.storage.settings.get('compact') || this.client.compact))
 		{
-			await message.clearReactions();
+			if (message.reactions.size > 0) await message.clearReactions();
 			CompactModeHelper.registerButton(
 				message,
 				this.client.buttons[options.button] || options.button,
