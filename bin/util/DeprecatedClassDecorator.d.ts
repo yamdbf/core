c34bf5ac3ccc43bb88d0522cd6dba0fba0674ff9
Zackary Campbell <zajrik@zajriksrv>
2018-05-12 11:51:28 +0200

    Build YAMDBF prebuilt: 6f9c6681f635adb83561eef439b9bf420d3d4a25

--- bin/util/DeprecatedClassDecorator.d.ts
+++ bin/util/DeprecatedClassDecorator.d.ts
@@ -1,2 +1,2 @@
 export declare function deprecatedClass(message: string): ClassDecorator;
-export declare function deprecatedClass<T>(target: T, key: PropertyKey): void;
+export declare function deprecatedClass<T>(target: T): T;
